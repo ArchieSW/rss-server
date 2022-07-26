ab7cbfc38438476de512dfa9c694515208045fe0
Kutuzov Dmitry <archie-woc@ya.ru>
2022-07-26 13:30:09 +0300

    LoggerService implementation

--- src/logger/logger.interface.ts
+++ src/logger/logger.interface.ts
@@ -1,6 +1,4 @@
-
 export default interface ILoggerService {
-    logger: unknown;
     log: (...args: unknown[]) => void;
     error: (...args: unknown[]) => void;
     warn: (...args: unknown[]) => void;
