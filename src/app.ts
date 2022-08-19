6be2c1ba39557555762ddbdb2c73709478ee228d
Kutuzov Dmitry <archie-woc@ya.ru>
2022-08-19 23:16:04 +0300

    Audit packages and using express json instead bodyparser

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import ILoggerService from './logger/logger.interface';
 import { TYPES } from './types';
 import UsersController from './users/users.controller';
 import ExceptionFilter from './errors/exception.filter';
-import { json } from 'body-parser';
 import PrismaService from './database/prisma.service';
 import AuthMiddleware from './common/auth.middleware';
 import ConfigService from './config/config.service';
@@ -40,7 +39,7 @@ export default class App {
     }
 
     public useMiddlewares(): void {
-        this._app.use(json());
+        this._app.use(express.json());
         const secret = this.configService.get('SECRET');
         const authMiddleware = new AuthMiddleware(secret);
         this._app.use(authMiddleware.execute.bind(authMiddleware));
